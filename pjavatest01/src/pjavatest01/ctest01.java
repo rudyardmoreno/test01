d202d91a0a0fc22ceed066beb869503778ec945d
rudyard.moreno <rudyard.moreno@MERL7-217385.echostar.com>
2017-03-13 11:00:09 -0600

    test v2

--- pjavatest01/src/pjavatest01/ctest01.java
+++ pjavatest01/src/pjavatest01/ctest01.java
@@ -2,6 +2,8 @@ package pjavatest01;
 
 public class ctest01 {
 	public static void main (String[] args){
-		System.out.println("Hello, World");
+		for (int i=1;i<=10;i++) {
+			System.out.println("Hello, World -->" + i);
+		}		
 	}
 }
